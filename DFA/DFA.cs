2d27c4b8c6aba0e0feb808d0bb5504a2804389b7
Massimo A. Santin <msantin@tempestive.com>
2021-12-19 21:04:17 +0100

    first commit

---
+++ DFA/DFA.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+
+namespace DFA
+{
+    /// <summary>
+    /// DFA base state
+    /// Every state is unique identified by a numeric integer Id
+    /// </summary>
+    [DataContract]
+    public abstract class DFAState : IComparable
+    {
+        /// <summary>
+        /// State ID
+        /// </summary>
+        [DataMember]
+        public int Id { get; set; }
+        /// <summary>
+        /// Action to execute when in the state
+        /// </summary>
+        [IgnoreDataMember]
+        public Action Action { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            if (Id < ((DFAState)obj).Id)
+            {
+                return -1;
+            }
+            else if (Id > ((DFAState)obj).Id)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+    }
+
+    /// <summary>
+    /// State comparer
+    /// Two states are the same when their ids are equals
+    /// </summary>
+    internal class DFAStateComparer : IComparer<DFAState>
+    {
+        public int Compare(DFAState x, DFAState y)
+        {
+            if (x.Id < y.Id)
+            {
+                return -1;
+            }
+            else if (x.Id > y.Id)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Serialization format
+    /// </summary>
+    public enum DFASerializationFormat { XML, JSON }
+
+    /// <summary>
+    /// DFA
+    /// </summary>
+    /// <typeparam name="TState"></typeparam>
+    [DataContract]
+    public abstract class DFA<TState> where TState : DFAState
+    {
+        [IgnoreDataMember]
+        private Dictionary<(int From, int To), Func<bool>> transitionTable = new Dictionary<(int From, int To), Func<bool>>();
+        [IgnoreDataMember]
+        private Dictionary<int, TState> statesById = new Dictionary<int, TState>();
+        [DataMember]
+        private SortedSet<TState> states = new SortedSet<TState>(new DFAStateComparer());
+        [DataMember]
+        private int currentStateId;
+
+        public IEnumerable<TState> States { get => states.AsEnumerable(); }
+        public TState CurrentState { get => statesById[currentStateId]; set => statesById[currentStateId] = value; }
+        public int CurrentStateId { get => currentStateId; set => currentStateId = value; }
+
+        public DFA()
+        {
+            CurrentState = null;
+            this.DefineStates();
+            this.DefineTransitions();
+        }
+
+        /// <summary>
+        /// BUild states
+        /// </summary>
+        public abstract void DefineStates();
+
+        /// <summary>
+        /// Build transition table
+        /// </summary>
+        public abstract void DefineTransitions();
+
+        #region Definition
+
+        public void AddState(int stateId, TState state)
+        {
+            statesById[stateId] = state;
+            state.Id = stateId;
+            states.Add(state);
+        }
+
+        public void AddState(TState state)
+        {
+            int stateId = state.Id;
+            statesById[stateId] = state;
+            states.Add(state);
+        }
+
+        public void AddTransitionLink(int from, int to, Func<bool> checkFunction)
+        {
+            transitionTable[(from, to)] = checkFunction;
+        }
+
+        public void AddTransitionLink(TState from, TState to, Func<bool> checkFunction)
+        {
+            transitionTable[(from.Id, to.Id)] = checkFunction;
+        }
+
+        #endregion
+
+        #region Execution
+
+        public void StartFrom(int fromId)
+        {
+            currentStateId = fromId;
+        }
+
+        public void StartFrom(TState from)
+        {
+            currentStateId = from.Id;
+        }
+
+        public bool CanMoveTo(TState to) => CanMoveTo(to.Id);
+
+        public bool CanMoveTo(int toId)
+        {
+            if (transitionTable.ContainsKey((currentStateId, toId)))
+            {
+                if (transitionTable[(currentStateId, toId)].Invoke())
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool MoveTo(TState to) => MoveTo(to.Id);
+
+        public bool MoveTo(int toId)
+        {
+            if (transitionTable.ContainsKey((currentStateId, toId)))
+            {
+                if (transitionTable[(currentStateId, toId)].Invoke())
+                {
+                    currentStateId = toId;
+                    CurrentState.Action.Invoke();
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public IEnumerable<TState> GetNextStatesEx()
+        {
+            List<TState> nextStates = new List<TState>();
+            foreach ((int From, int To) k in transitionTable.Keys)
+            {
+                if (k.From == currentStateId)
+                {
+                    nextStates.Add(statesById[k.To]);
+                }
+            }
+            return nextStates;
+        }
+
+
+        public IEnumerable<int> GetNextStates()
+        {
+            List<int> nextStates = new List<int>();
+            foreach (var k in transitionTable.Keys)
+            {
+                if (k.From == currentStateId)
+                {
+                    nextStates.Add(k.To);
+                }
+            }
+            return nextStates;
+        }
+
+        public bool Move()
+        {
+            IEnumerable<int> nexts = GetNextStates();
+            if (!nexts.Any())
+            {
+                return false;
+            }
+            foreach (int next in nexts)
+            {
+                if (CanMoveTo(next))
+                {
+                    MoveTo(next);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        #endregion
+
+        #region Save/Load Memory
+
+        public void Save(Stream output, DFASerializationFormat format = DFASerializationFormat.XML)
+        {
+            switch (format)
+            {
+                case DFASerializationFormat.XML:                    ;
+                    DataContractSerializer serializer = new DataContractSerializer(GetType());
+                    serializer.WriteObject(output, this);
+                    break;
+                case DFASerializationFormat.JSON:
+                    throw new NotImplementedException($"Format {format} not yet implemented");
+                default:
+                    throw new SerializationException($"Wrong format {format}");
+            }
+        }
+
+        public static TDFA Load<TDFA>(Stream input, DFASerializationFormat format = DFASerializationFormat.XML) where TDFA : DFA<TState>, new()
+        {
+            switch (format)
+            {
+                case DFASerializationFormat.XML:
+                    DataContractSerializer serializer = new DataContractSerializer(typeof(TDFA));
+                    TDFA dfa = (TDFA)serializer.ReadObject(input);
+                    TDFA tempDfa = new TDFA();
+                    foreach (TState st in dfa.States)
+                    {
+                        st.Action = tempDfa.States.Where(tempSt => tempSt.Id == st.Id).First().Action;
+                    }
+                    if (dfa.States.Any(st => st.Id == dfa.CurrentState.Id))
+                    {
+                        dfa.CurrentState = dfa.States.Where(st => st.Id == dfa.CurrentState.Id).First();
+                    }
+                    return dfa;
+                case DFASerializationFormat.JSON:
+                    throw new NotImplementedException($"Format {format} not yet implemented");
+                default:
+                    throw new SerializationException($"Wrong format {format}");
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ DFA/Program.cs
@@ -0,0 +1,148 @@
+﻿using Spectre.Console;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+
+namespace DFA
+{
+    [DataContract]
+    public class MyStato : DFAState
+    {
+        [DataMember]
+        public double v;
+    }
+
+    /// <summary>
+    /// DFA
+    /// 
+    ///   [[START]] --> [FIRST_STEP] -+-if(valore is odd)---> [ODD] --+-> [LAST_STEP] --> [[FINISH]]
+    ///                               |                               |
+    ///                               |                               |
+    ///                               +-if(valore is even)--> [EVEN] -+
+    ///
+    /// State Actions
+    /// 
+    ///   [[START]]    ==> nothing
+    ///   [FIRST_STEP] ==> nothing
+    ///   [ODD]        ==> print DISPARI
+    ///   [EVEN]       ==> print PARI
+    ///   [FIRST_STEP] ==> nothing
+    ///   [[FINISH]]   ==> nothing
+    ///                                
+    /// </summary>
+    public class MyMacchina : DFA<MyStato>
+    {
+        public int valore = 0;
+        public int START = 0, FIRST_STEP = 1, IS_EVEN = 2, IS_ODD = 3, LAST_STEP = 4, FINISH = 5;
+
+        public override void DefineStates()
+        {
+            AddState(START, new MyStato() { v = 10.0, Action = () => Log($"Inizio: sono nello stato {CurrentState.v}") });
+            AddState(FIRST_STEP, new MyStato() { v = 10.1, Action = () => Log($"Primo passo: sono nello stato {CurrentState.Id}. valore: {valore}") });
+            AddState(IS_EVEN, new MyStato()
+            {
+                v = 10.2,
+                Action = () =>
+                {
+                    Log($"PARI: sono nello stato {CurrentState.Id}. valore: {valore}");
+                    AnsiConsole.WriteLine($"Il numero {this.valore} è");
+                    AnsiConsole.Render(new FigletText($"PARI").LeftAligned().Color(Color.Green));
+                },
+            });
+            AddState(IS_ODD, new MyStato()
+            {
+                v = 10.3,
+                Action = () =>
+                {
+                    Log($"DISPARI: sono nello stato {CurrentState.Id}. valore: {valore}");
+                    AnsiConsole.WriteLine($"Il numero {this.valore} è");
+                    AnsiConsole.Render(new FigletText($"DISPARI").LeftAligned().Color(Color.Green));
+                },
+            });
+            AddState(LAST_STEP, new MyStato() { v = 10.3, Action = () => Log($"Ultimo passo: sono nello stato {CurrentState.Id}. valore: {valore}") });
+            AddState(FINISH, new MyStato() { v = 10.5, Action = () => Log($"Finito: sono nello stato {CurrentState.Id}. valore: {valore}") });
+        }
+
+        public override void DefineTransitions()
+        {
+            AddTransitionLink(START, FIRST_STEP, () => true);
+            AddTransitionLink(FIRST_STEP, IS_EVEN, () => valore % 2 == 0);
+            AddTransitionLink(FIRST_STEP, IS_ODD, () => valore % 2 != 0);
+            AddTransitionLink(IS_EVEN, FINISH, () => true);
+            AddTransitionLink(IS_ODD, FINISH, () => true);
+        }
+
+        private static void Log(string logString)
+        {
+            AnsiConsole.MarkupLine($"[red]{logString}[/]");
+        }
+
+    }
+
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            AnsiConsole.WriteLine("Hello DFA World!");
+
+            string fileName = Path.GetTempFileName();
+            AnsiConsole.MarkupLine($"Temporary file: {fileName}");
+
+            {
+                int? number = null; ;
+                while (!number.HasValue)
+                {
+                    try
+                    {
+                        AnsiConsole.MarkupLine("[yellow]Write a number and press <ENTER>[/]");
+                        string numberAsString = Console.ReadLine();
+                        number = int.Parse(numberAsString);
+                    }
+                    catch (FormatException) { }
+                }
+                MyMacchina macchina = new MyMacchina();
+                macchina.valore = number.Value;
+                macchina.StartFrom(macchina.START);
+                // go to next state
+                macchina.Move();
+
+                // save dfa
+                using (FileStream outf = new FileStream(fileName, FileMode.Create, FileAccess.ReadWrite, FileShare.Read))
+                {
+                    macchina.Save(outf);
+                }
+                AnsiConsole.WriteLine("DFA saved");
+            }
+            {
+                // reload the dfa (in a different empty variable)
+                MyMacchina macchina2;
+                using (FileStream inf = new FileStream(fileName, FileMode.Open, FileAccess.ReadWrite, FileShare.Read))
+                {
+                    macchina2 = MyMacchina.Load<MyMacchina>(inf);
+                }
+                AnsiConsole.WriteLine("DFA loaded");
+                while (macchina2.CurrentStateId != macchina2.FINISH)
+                {
+                    IEnumerable<int> nexts = macchina2.GetNextStates();
+                    if (!nexts.Any())
+                    {
+                        throw new ApplicationException($"Bad designed DFA: there aren't any transition from {macchina2.CurrentState.Id}");
+                    }
+                    foreach (int next in nexts)
+                    {
+                        if (macchina2.CanMoveTo(next))
+                        {
+                            macchina2.MoveTo(next);
+                            break;
+                        }
+                    }
+                }
+            }
+
+            File.Delete(fileName);
+        }
+    }
+}
